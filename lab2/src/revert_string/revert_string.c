5e300487389d79310140293823d8b5e528a3a1b9
vsemenkovv <v.semenkov.v@gmail.com>
2019-03-15 13:45:29 +0000

    первое и второе сделано

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include  <string.h>
+#include ‘<stdlib.h>’
 
 void RevertString(char *str)
 {
-	// your code here
+
+	int N = strlen(str) - 1;
+	char *buf =  malloc(sizeof(char) * (strlen(str)+1));
+	strcpy(buf, str);
+	for (int i = 0; i < N+1; i++) {
+		strncpy((buf + i), (str + N - i), 1);
+	}
+	strcpy(str,buf);
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,7 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+	char buf = *left;
+   *left = *right;
+   *right = buf;
 }
